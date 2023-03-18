8e9e2c90dec63d57647f79f17608654accdec741
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-03-18 17:11:05 -0300

    settings verif

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6$1hpki8#ycnsbuml!x$-c94-kpoeaie=9t%fpdps=7#jq!nm%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
